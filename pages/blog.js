84b77c55cb149b5858986ff8881f4bcc84c24030
chandanikhanesha <chandanikhanesha@gmail.com>
2022-10-16 19:08:34 +0530

    fix build bug

--- pages/blog.js
+++ pages/blog.js
@@ -1,16 +1,7 @@
-// import React,{useEffect} from 'react'
-// import axios from 'axios'
-// var demo;
+import React from 'react'
 
-// export default function blog() {
-
-//     useEffect(() => {
-//       axios.get('http://admin.ilovecompress.appskym.com/api/blogs').then((res)=>{
-//         console.log( typeof res.data.data.data[0].description,"res");
-//         demo=res.data.data.data[0].description
-//       })
-    
-//     }, [])
-//     console.log(typeof demo,"demo");
-//   return demo;
-// }
+export default function blog() {
+  return (
+    <div>blog</div>
+  )
+}
