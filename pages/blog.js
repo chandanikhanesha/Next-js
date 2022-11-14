2133d990a308437d030aa46d7b929aeeef89afa3
chandanikhanesha <chandanikhanesha@gmail.com>
2022-11-14 22:28:54 +0530

    added blog changes

--- pages/blog.js
+++ pages/blog.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React from "react";
 import DrawerAppBar from "./navbar";
 import styles from "../styles/Home.module.css";
-
-
+import Axios from "axios";
+import Loader from "../components/loader";
 
 export default function blog() {
+  const [blogData, setblogData] = React.useState("");
+  React.useEffect(() => {
+    Axios.get("http://admin.ilovecompress.appskym.com/api/blogs").then(
+      (res) => {
+        // console.log(res.data.data.data[0], "res");
+        setblogData(res.data.data.data[0].description);
+      }
+    );
+  }, []);
+
   return (
     <div>
-      <DrawerAppBar/>
+      <DrawerAppBar />
+      <div className={styles.outerCard}>
+        <div className={styles.anothercard}>
+          {blogData == "" ? (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+            <Loader />
+            </div>
+          ) : (
+            <div>
+              <h2>Methods to Compress JPEG under 50KB</h2>
+              <div
+                className={styles.blogCard}
+                dangerouslySetInnerHTML={{ __html: blogData }}
+              />
+            </div>
+          )}
+        </div>
+      </div>
       <footer className={styles.footer}>
         <div className={styles.likeText}>Like It ! Share It</div>
-        {/* <div className={styles.footerText}>
-          All uploaded data is deleted after refreshing
-        </div> */}
         <div className={styles.copyrightText}>
           Copyright @ Image Compressor {new Date().getFullYear()}. All Rights
           Reserved.
         </div>
       </footer>
     </div>
-  )
+  );
 }
